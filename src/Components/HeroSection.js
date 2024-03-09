c268a373f4669ecf8f3e21e0c9251ea729a0bc07
Armaan <indaniarmaan4@gmail.com>
2024-03-09 10:30:56 +0530

    ---

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ctimage from "../assets/images/clinical-trial-removebg-preview.png";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div className="hero-section-text">
+        <h1>
+          Welcome to <br></br>ClinicAll Hub!
+        </h1>
+        <p>Empowering Patients. Advancing Medicine. Together.</p>
+        <div className="contact-us-button">Contact Us</div>
+      </div>
+      <img src={ctimage} alt="" />
+    </div>
+  );
+};
+
+export default HeroSection;

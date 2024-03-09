572c81ff7b3feaeaa3806e481bc83a1c79863f4b
Akshat Bhatia <akshatrakesh.bhatia2022@vitstudent.ac.in>
2024-03-09 11:09:24 +0530

    navbar done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import './Navbar.css';
+import logo from '../assets/images/CLINICALL_LOGO.png'
+
 const Navbar = () => {
   return (
-    <>
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
-
-    </>
+    <div className="bg">
+      <div className="navbar">
+        <div className="left-section">
+          <img src={logo} alt="Logo" className="logo" /> {/* Add logo here */}
+          <a href="/">Clinic All</a>
+        </div>
+        <div className="right-section">
+          <ul>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/faq">FAQ</a></li>
+            <li><button type="button" onClick={() => {window.location.href = '/contact'}}>Contact</button></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 

149df690d0d1a00f35cbb7630fa4bdc8e19563a3
Akshat Bhatia <akshatrakesh.bhatia2022@vitstudent.ac.in>
2024-03-09 14:11:00 +0530

    login form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import HeroSection from "./Components/HeroSection";
 import About from "./Components/About";
+import Login from "./Components/Login";
+import React, { useState } from "react";
 
 function App() {
+
+
+  const [showLoginForm, setShowLoginForm] = useState(false);
+
+  const handleContactUsClick = () => {
+    setShowLoginForm(true);
+  };
+
+  const handleCloseLoginForm = () => {
+    setShowLoginForm(false);
+  };
+
+
   return (
     <>
-      <Navbar />
+       <Navbar onContactUsClick={handleContactUsClick} />
+      {showLoginForm && <Login onClose={handleCloseLoginForm} />}
       <HeroSection></HeroSection>
       <About />
+     
     </>
   );
 }

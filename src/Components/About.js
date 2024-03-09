d01df6275e5f7f0b29e5abe67debcb1b21e4ad89
Aaryan <aaryannarang222@gmail.com>
2024-03-09 17:51:19 +0530

    about changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,12 +4,12 @@ import "./About.css";
 const About = () => {
   return (
     <div className="about-container">
-      <div className="left">
-        <h1>All About ClinicAll</h1>
+      <div className="top">
+        <h1>About Clinic<span>ALL</span></h1>
       </div>
-      <div className="right">
+      <div className="bottom">
         <p>
-          At ClinicAll, we understand the challenges individuals face when seeking treatment options for various health conditions. That's why we're dedicated to connecting patients with cutting-edge drug trials, offering hope and opportunities for healing while contributing to the advancement of medical science.
+          At ClinicALL, we understand the challenges individuals face when seeking treatment options for various health conditions. That's why we're dedicated to connecting patients with cutting-edge drug trials, offering hope and opportunities for healing while contributing to the advancement of medical science.
         </p>
         <p>
           Our platform serves as a bridge between patients and pharmaceutical researchers, providing a seamless experience for those seeking innovative treatment options.
